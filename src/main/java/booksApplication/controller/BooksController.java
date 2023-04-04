8dd8fd73fe4272044cb879bde92ac7b55281a9d1
franciellyl <franciellyelima@gmail.com>
2023-04-04 20:53:42 -0300

    WIP: Developer get /volume endpoint and api health check.

--- src/main/java/booksApplication/controller/BooksController.java
+++ src/main/java/booksApplication/controller/BooksController.java
@@ -5,29 +5,44 @@ import booksApplication.model.request.BooksRequest;
 import booksApplication.model.response.BooksResponse;
 import booksApplication.service.impl.BooksServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.context.annotation.Description;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+import java.net.*;
 
 @RestController
+@RequestMapping("/books/")
+@SuppressWarnings("all")
 public class BooksController {
     @Autowired
     BooksServiceImpl booksService;
 
     @Autowired
-    BooksResponse booksResponse;
+    URLConfig urlConfig;
 
-    @GetMapping(value = "/volume")
-    public ResponseEntity<BooksResponse> getVolume(BooksRequest booksRequest){
-        booksService.processor(booksRequest);
-        return new ResponseEntity<>(booksResponse, HttpStatusCode.valueOf(200));
+    @GetMapping(value = "/volumes")
+    @Description("Search any volume using the book param.")
+    public ResponseEntity<String> getVolume(@RequestParam String book){
+        String body = booksService.processor(book);
+        return new ResponseEntity<>(body, HttpStatusCode.valueOf(200));
     }
 
     @GetMapping(value = "/healthcheck")
-    public ResponseEntity<String> healthCheck(){
-        return new ResponseEntity<>("Application Up.", HttpStatus.valueOf(200));
+    @Description("Application health check")
+    public ResponseEntity<Health> healthCheck() throws IOException {
+       String url = new URL(urlConfig.getUrl()).getHost();
+        try(Socket socket =
+                    SSLSocketFactory.getDefault().createSocket(url, 443)) {
+        } catch (MalformedURLException | NoRouteToHostException | UnknownHostException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Health.down().withDetail("Error: ", e.getMessage()).build());
+        }
+        return ResponseEntity.ok(Health.up().build());
     }
 }
--- src/main/java/booksApplication/service/impl/BooksServiceImpl.java
+++ src/main/java/booksApplication/service/impl/BooksServiceImpl.java
@@ -5,16 +5,34 @@ import booksApplication.model.request.BooksRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @Service
 public class BooksServiceImpl {
 
     @Autowired
     URLConfig urlConfig;
 
-    public void processor(BooksRequest booksRequest){
-        String url = urlConfig.getUrl() + "/volumes?" + booksRequest.getQ();
-        System.out.printf(url);
+    public String processor(String book){
+        String body = null;
+        try {
+            //connection
+            String url = urlConfig.getUrl() + "/volumes?q=" + book;
+            URI path = URI.create(url);
 
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder(path).GET().build();
+
+            //response api
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            body = response.body();
+        }catch(Exception e){
+            System.out.println("Message Error:" + e);
+        }
+        return body;
     }
 
 }
